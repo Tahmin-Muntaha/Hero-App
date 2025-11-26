4192ddf9e9ce42adb69613fd0992e133ccd31924
Tahmin-Muntaha <tahminmuntaha66@gmail.com>
2025-11-26 15:24:16 +0600

    final

--- src/Components/AllApss/AllApps.jsx
+++ src/Components/AllApss/AllApps.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData, useNavigation } from 'react-router-dom';
 import App from '../App/App';
 
 
 const AllApps = () => {
     const allAppsData=useLoaderData()
+    console.log(allAppsData)
     const navigation=useNavigation()
+    const [search,setSearch]=useState('')
+    const handleSearch=(e)=>{
+        
+        setSearch(e.target.value)
+        
+
+
+    }
+    const filteredApp=allAppsData.filter(a=>a.title.toLowerCase().includes(search.toLowerCase()))
+    console.log(filteredApp)
     if(navigation.state==='loading'){
         return (
             <div className="flex justify-center items-center h-[400px]">
@@ -21,7 +32,7 @@ const AllApps = () => {
             <p className='text-center'><small>Explore All Apps on the Market developed by us. We code for Millions</small></p>
             <div className='flex justify-between items-center'>
                 <p>{allAppsData.length} Apps Found</p>
-                <input name="name" type="text" placeholder='search' className='border border-gray-400 p-2'></input>
+                <input name="name" type="text" placeholder='search' className='border border-gray-400 p-2' onChange={handleSearch}></input>
             </div>
             <div className='grid grid-cols-2 md:grid-cols-3 gap-5'>
             {
